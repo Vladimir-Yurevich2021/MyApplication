2cec3a7cccc6778047a96482e8221bf1328f4550
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-12 18:29:59 +0300

    добавили в MainActivity.java кнопки

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.myapplication;
 
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+private String tb_0;
+private String tb_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_0=findViewById(R.id.b_0);
+        Button button_1=findViewById(R.id.b_1);
+        Button button_2=findViewById(R.id.b_2);
+        Button button_3=findViewById(R.id.b_3);
+        Button button_4=findViewById(R.id.b_4);
+        Button button_5=findViewById(R.id.b_5);
+        Button button_6=findViewById(R.id.b_6);
+        Button button_7=findViewById(R.id.b_7);
+        Button button_8=findViewById(R.id.b_8);
+        Button button_9=findViewById(R.id.b_9);
+        Button button_c=findViewById(R.id.b_c);
+        Button button_dec=findViewById(R.id.b_dec);
+        Button button_delete=findViewById(R.id.b_delete);
+        Button button_del=findViewById(R.id.b_del);
+        Button button_point=findViewById(R.id.b_point);
+        Button button_proc=findViewById(R.id.b_proc);
+        Button button_ravn=findViewById(R.id.b_ravn);
+        Button button_s=findViewById(R.id.b_s);
+        Button button_sum=findViewById(R.id.b_sum);
+
+
     }
 }
