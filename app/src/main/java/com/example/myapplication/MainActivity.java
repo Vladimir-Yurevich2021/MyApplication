5368df99ec2d507a2d30bd274d935401007abcd4
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-16 09:46:57 +0300

    Добали методы инициализации в активити

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private Numbers numbers;
+private TextView text_enter;
+private TextView text_rezult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        numbers=new Numbers();
+initView();
     }
+
+    private void initView() {
+    text_enter=findViewById(R.id.tv_enter_number);
+    text_rezult=findViewById(R.id.rezult);
+    initButton();
+    }
+
+    private void initButton() {
+    }
+
 }
