cb04c9b9f8e90c2daedd5453d946d45df02c53d6
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-16 09:39:31 +0300

    сделали класс наследуюемым для парсериализации. Добавили геттеры и сеттеры

--- app/src/main/java/com/example/myapplication/Numbers.java
+++ app/src/main/java/com/example/myapplication/Numbers.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
-public class Numbers {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Numbers implements Parcelable {
     private int number1;
     private int number2;
     private int number_rezult;
@@ -11,6 +14,49 @@ public class Numbers {
         number_rezult=0;
 
     }
+
+    public int getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int getNumber_rezult() {
+        return number_rezult;
+    }
+
+    public void setNumber_rezult(int number_rezult) {
+        this.number_rezult = number_rezult;
+    }
+
+    protected Numbers(Parcel in) {
+        number1 = in.readInt();
+        number2 = in.readInt();
+        number_rezult = in.readInt();
+    }
+
+    public static final Creator<Numbers> CREATOR = new Creator<Numbers>() {
+        @Override
+        public Numbers createFromParcel(Parcel in) {
+            return new Numbers(in);
+        }
+
+        @Override
+        public Numbers[] newArray(int size) {
+            return new Numbers[size];
+        }
+    };
+
     public void addition(){
         number_rezult=number1+number2;
     }
@@ -26,5 +72,17 @@ public class Numbers {
         if (number1!=0||number2!=0){
             number_rezult=number1/number2;
         }else number_rezult=0;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(number1);
+        parcel.writeInt(number2);
+        parcel.writeInt(number_rezult);
     }
 }
