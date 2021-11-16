d36353d486b3cd5ce2bdbe0b47b9da9fed3c21df
Vladimir_Yurevich <dvagig@gmail.com>
2021-11-16 09:31:42 +0300

    добавили поля под хранения чисел и обозначили методы с ними

--- app/src/main/java/com/example/myapplication/Numbers.java
+++ app/src/main/java/com/example/myapplication/Numbers.java
@@ -1,4 +1,18 @@
 package com.example.myapplication;
 
 public class Numbers {
+    private int number1;
+    private int number2;
+    private int number_rezult;
+
+    Numbers(){
+        number1=0;
+        number2=0;
+        number_rezult=0;
+
+    }
+    public void addition(){}
+    public void subtraction(){}
+    public void multiplication(){}
+    public void division(){}
 }
